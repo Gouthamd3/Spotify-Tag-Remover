c5b65ec1f60ab98e8c088f42962440189ce39b30
Gouthamd3 <113016894+Gouthamd3@users.noreply.github.com>
2024-11-17 11:23:16 +0530

    Update File Renamer.py

--- File Renamer.py
+++ File Renamer.py
@@ -13,11 +13,32 @@ print("Files in '", path, "' :")
 print(dir_list)
 
 file_type = ".mp3"
-dat_remove = "[SPOTIFY-DOWNLOADER.COM] "
+dat_remove = "[iSongs.info]"
 #renaming the files
-for i in dir_list:
-    if file_type in i:
-        print(i)
-        if i[0:len(dat_remove)] == dat_remove:
-            os.renames(i,i[len(dat_remove):])
-            print(i[len(dat_remove):], "Done!!")
+def text_remover():
+    global file_type, dat_remove
+    
+    for i in dir_list:
+        if file_type in i:
+            print(i)
+            if i[0:len(dat_remove)] == dat_remove:
+                try:
+                    if dat_remove+file_type in dir_list: # for skipping duplicates
+                        i+1
+                    else:
+                        os.renames(i,i[len(dat_remove):])
+                        print(i[len(dat_remove):], "Done!!")
+                except:
+                    IndexError
+
+#removing numbers
+
+no_remove = "0"
+def number_remover():
+    global no_remove
+    for i in dir_list:
+        if file_type in i:
+            print(i)
+            if i[0:len(dat_remove)] == dat_remove:
+                pass
+
